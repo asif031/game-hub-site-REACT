80c50eb21f6cbe435f907995c024ae72f5cd60f3
unknown <asifuddaulaasif@gmail.com>
2024-11-14 10:00:39 +0600

    NavBar Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
 import { useState } from "react";
 import SearchBar from "./components/SearchBar";
+import { Button } from "./components/ui/button";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./componentsNative/NavBar";
 
 function App() {
   const [isDark, setIsDark] = useState(false);
 
   return (
-    <div className="App">
-      {isDark && (document.body.style.backgroundColor = "black")}
-      <SearchBar onToggle={() => setIsDark(!isDark)} />
-    </div>
+    <>
+      <div>
+        <Grid
+          templateAreas={{
+            base: `"nav" "main"`,
+            lg: `"nav nav" "aside main"`,
+          }}
+        >
+          <GridItem area="nav">
+            <NavBar />
+          </GridItem>
+          <Show when="lg">
+            <GridItem area="aside" bg="gold">
+              Aside
+            </GridItem>
+          </Show>
+
+          <GridItem area="main" bg="dodgerblue">
+            Main
+          </GridItem>
+        </Grid>
+      </div>
+      <div className="App">
+        {/* {isDark && (document.body.style.backgroundColor = "black")} */}
+        <SearchBar onToggle={() => setIsDark(!isDark)} />
+      </div>
+    </>
   );
 }
 
